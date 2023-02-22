02f4007d85d7587fd1a8669d48629053fd14b828
erischilde <126109436+erischilde@users.noreply.github.com>
2023-02-22 12:56:47 -0800

    Update pins_BTT_SKR_MINI_E3_V3_0.h
    Changes per https://www.smith3d.com/guide-to-connect-btt-mini-12864-to-skr-mini-e3-v3/

--- Marlin/src/pins/stm32g0/pins_BTT_SKR_MINI_E3_V3_0.h
+++ Marlin/src/pins/stm32g0/pins_BTT_SKR_MINI_E3_V3_0.h
@@ -312,48 +312,46 @@
 
     #elif ENABLED(FYSETC_MINI_12864_2_1)
 
-      #ifndef NO_CONTROLLER_CUSTOM_WIRING_WARNING
-        #error "CAUTION! FYSETC_MINI_12864_2_1 and clones require wiring modifications. See 'pins_BTT_SKR_MINI_E3_V3_0.h' for details. Define NO_CONTROLLER_CUSTOM_WIRING_WARNING to suppress this warning."
-      #endif
+	//#error "CAUTION! FYSETC_MINI_12864_2_1 and clones require wiring modifications. See 'pins_BTT_SKR_MINI_E3_V3_0.h' for details. Comment out this line to continue."
 
-      /**
-       *
-       *                 Board                               Display
-       *                 ------                               ------
-       *    (EN2)  PB5  | 1  2 | PA15(BTN_ENC)            5V |10  9 | GND
-       *  (LCD_CS) PA9  | 3  4 | RST (RESET)              -- | 8  7 | --
-       *  (LCD_A0) PA10   5  6 | PB9 (EN1)            (DIN)  | 6  5   (RESET)
-       *  (LCD_SCK)PB8  | 7  8 | PD6 (MOSI)         (LCD_A0) | 4  3 | (LCD_CS)
-       *            GND | 9 10 | 5V                (BTN_ENC) | 2  1 | --
-       *                 ------                               ------
-       *                  EXP1                                 EXP1
-       *
-       *                                                      ------
-       *                                                  -- |10  9 | --
-       *                   ---                       (RESET) | 8  7 | --
-       *                  | 3 |                      (MOSI)  | 6  5   (EN2)
-       *                  | 2 | (DIN)                     -- | 4  3 | (EN1)
-       *                  | 1 |                     (LCD_SCK)| 2  1 | --
-       *                   ---                                ------
-       *                Neopixel                               EXP2
-       *
-       * Needs custom cable. Connect EN2-EN2, LCD_CS-LCD_CS and so on.
-       *
-       * Check the index/notch position twice!!!
-       * On BTT boards pins from IDC10 connector are numbered in unusual order.
-       */
-      #define BTN_ENC                EXP1_02_PIN
-      #define BTN_EN1                EXP1_06_PIN
-      #define BTN_EN2                EXP1_01_PIN
-      #define BEEPER_PIN                    -1
+  /**
+   *
+   *                 Board                               Display
+   *                 ------                               ------
+   *    (EN2)  PB5  |10  9 | PA15(BTN_ENC)            5V |10  9 | GND
+   *  (LCD_CS) PA9  | 8  7 | RST (RESET)              -- | 8  7 | --
+   *  (LCD_A0) PA10 |#6  5 | PB9 (EN1)            (DIN)  | 6  5#| (RESET)
+   *  (LCD_SCK)PB8  | 4  3 | PD6 (MOSI)         (LCD_A0) | 4  3 | (LCD_CS)
+   *            GND | 2  1 | 5V                (BTN_ENC) | 2  1 | --
+   *                 ------                               ------
+   *                  EXP1                                 EXP1
+   *
+   *                                                      ------
+   *                                                  -- |10  9 | --
+   *                   ---                       (RESET) | 8  7 | --
+   *                  | 3 |                      (MOSI)  | 6  5#| (EN2)
+   *                  | 2 | (DIN)                     -- | 4  3 | (EN1)
+   *                  | 1 |                     (LCD_SCK)| 2  1 | --
+   *                   ---                                ------
+   *                Neopixel                               EXP2
+   *
+   * Needs custom cable. Connect EN2-EN2, LCD_CS-LCD_CS and so on.
+   *
+   * Check twice index position!!! (marked as # here)
+   * On BTT boards pins from IDC10 connector are numbered in unusual order.
+   */
+#define BTN_ENC                  EXP1_09_PIN
+#define BTN_EN1                         PB9
+#define BTN_EN2                         PB5
+#define BEEPER_PIN                      -1
 
-      #define DOGLCD_CS              EXP1_03_PIN
-      #define DOGLCD_A0              EXP1_05_PIN
-      #define DOGLCD_SCK             EXP1_07_PIN
-      #define DOGLCD_MOSI            EXP1_08_PIN
+#define DOGLCD_CS                       PA9
+#define DOGLCD_A0                       PA10
+#define DOGLCD_SCK                      PB8
+#define DOGLCD_MOSI                     PD6
 
-      #define FORCE_SOFT_SPI
-      #define LCD_BACKLIGHT_PIN             -1
+#define FORCE_SOFT_SPI
+#define LCD_BACKLIGHT_PIN               -1
 
     #else
       #error "Only CR10_STOCKDISPLAY, ZONESTAR_LCD, ENDER2_STOCKDISPLAY, MKS_MINI_12864, FYSETC_MINI_12864_2_1, and TFTGLCD_PANEL_(SPI|I2C) are currently supported on the BIGTREE_SKR_MINI_E3."
@@ -366,7 +364,7 @@
 #if BOTH(TOUCH_UI_FTDI_EVE, LCD_FYSETC_TFT81050)
 
   #ifndef NO_CONTROLLER_CUSTOM_WIRING_WARNING
-    #error "CAUTION! LCD_FYSETC_TFT81050 requires wiring modifications. See 'pins_BTT_SKR_MINI_E3_common.h' for details. (Define NO_CONTROLLER_CUSTOM_WIRING_WARNING to suppress this warning.)"
+    //#error "CAUTION! LCD_FYSETC_TFT81050 requires wiring modifications. See 'pins_BTT_SKR_MINI_E3_common.h' for details. (Define NO_CONTROLLER_CUSTOM_WIRING_WARNING to suppress this warning.)"
   #endif
 
   /**
